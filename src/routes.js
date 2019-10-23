d4301875536a96a3dd2b4e5a23407cbbcd0f4163
VictorHCord <victorhugocordeiro1997@gmail.com>
2019-10-23 19:19:08 -0300

    Criando autenticação das minhas rotas

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,16 @@ import { Router } from 'express';
 import StudentController from './app/controllers/StudentController';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.post('/students', StudentController.store);
-routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+routes.post('/users', UserController.store);
+
+routes.use(authMiddleware);
+
+routes.post('/students', StudentController.store);
+routes.put('/students', StudentController.update);
 
 export default routes;
